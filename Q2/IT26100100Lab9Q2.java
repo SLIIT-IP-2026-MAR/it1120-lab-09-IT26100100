1dea07ef8665ca10931882e89e06e00d9665d9db
IT26100100 <IT26100100@my.sliit.lk>
2026-06-26 19:03:13 +0530

    Add files via upload

---
+++ Q2/IT26100100Lab9Q2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class IT26100100Lab9Q2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the radius of the circle: ");
+        double radius = sc.nextDouble();
+
+        double area = circleArea(radius);
+        System.out.println("The area of the circle with radius " + radius + " is : " + area);
+    }
+
+    // User-defined method
+    public static double circleArea(double r) {
+        return Math.PI * r * r;
+    }
+}
